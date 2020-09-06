b9d512ccaac174b3960cdc2c4df4b10b2d334284
kami988 <baske.yoshi007@gmail.com>
2020-09-06 14:32:00 +0000

    vue add apollo

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+// @ts-ignore
+import { createProvider } from "./vue-apollo";
 
 Vue.config.productionTip = false;
 
+// @ts-ignore
 new Vue({
   router,
   store,
+  // @ts-ignore
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount("#app");
