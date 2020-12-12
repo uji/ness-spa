2fbe5a2f5e86c308bc9076e85428c2fba31a4404
uji <ujiprog@gmail.com>
2020-12-12 13:50:48 +0000

    feat: add prod url to client.ts

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -2,8 +2,11 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+const host = 'http://localhost:3000';
+// const host = 'https://55m2930t23.execute-api.ap-northeast-1.amazonaws.com/prod';
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3000/query',
+  uri: host + '/query',
   credentials: 'include'
 });
 const cache = new InMemoryCache();
