af3323073550332058a3b4b7b5b8f972794efd97
uji <ujiprog@gmail.com>
2020-12-26 16:27:03 +0000

    fix: move initialize process

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import router from './router';
 import store from './store';
 import { DefaultApolloClient } from '@vue/apollo-composable';
 import { apolloClient } from '@/graphql/client';
+import { firebaseConfig } from '@/firebase/config'
+import * as firebase from 'firebase/app';
+import * as firebaseui from 'firebaseui';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize the FirebaseUI Widget using Firebase.
+export const authUI = new firebaseui.auth.AuthUI(firebase.auth());
 
 createApp(App)
   .use(store)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,48 +10,14 @@ import { defineComponent, onMounted } from 'vue';
 
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
-import * as firebaseui from 'firebaseui';
-import { firebaseConfig } from '@/firebase/config'
-// import { CookieWriteConverter } from '@types/js-cookie';
-// import * as Cookies from '@types/js-cookie';
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Initialize the FirebaseUI Widget using Firebase.
-const ui = new firebaseui.auth.AuthUI(firebase.auth());
-firebaseui.auth.AuthUI;
-const uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult: any, redirectUrl: any) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    }
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'redirect',
-  signInSuccessUrl: '/',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>',
-  // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
-};
+import { authUI } from '@/main';
+import { uiConfig } from '@/firebase/ui'
 
 export default defineComponent({
   setup() {
     onMounted(() => {
-      ui.start('#firebaseui-auth-container', {
+      // TODO: UIちゃんと作る
+      authUI.start('#firebaseui-auth-container', {
         signInOptions: [
           {
             provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
@@ -59,8 +25,7 @@ export default defineComponent({
           }
         ]
       });
-      // The start method will wait until the DOM is loaded.
-      // ui.start('#firebaseui-auth-container', uiConfig);
+      // authUI.start('#firebaseui-auth-container', uiConfig);
     });
     firebase.auth().onAuthStateChanged(async (user) => {
       if (user == null) {
