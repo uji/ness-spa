42d5601d945f2520cff7cc302b424e9dc6f2330c
uji <ujiprog@gmail.com>
2020-12-26 15:52:34 +0000

    fix: move firebase config to other file

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,21 +11,10 @@ import { defineComponent, onMounted } from 'vue';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import * as firebaseui from 'firebaseui';
+import { firebaseConfig } from '@/firebase/config'
 // import { CookieWriteConverter } from '@types/js-cookie';
 // import * as Cookies from '@types/js-cookie';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyDoxQU1-q6IrXoo9lPHH8wLBGm4dr5BA2w',
-  authDomain: 'ness-85856.firebaseapp.com',
-  databaseURL: 'https://ness-85856.firebaseio.com',
-  projectId: 'ness-85856',
-  storageBucket: 'ness-85856.appspot.com',
-  messagingSenderId: '410365716960',
-  appId: '1:410365716960:web:01fb9a4a9869527b2e2385',
-  measurementId: 'G-E34BNZV0JP'
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
