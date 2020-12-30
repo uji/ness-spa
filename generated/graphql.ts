fa01d38f6fc9b3d51c39ed52d4854e85b2eb777d
uji <ujiprog@gmail.com>
2020-12-30 19:53:40 +0000

    fix: apollo settings

--- generated/graphql.ts
+++ generated/graphql.ts
@@ -1,8 +1,11 @@
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import gql from 'graphql-tag';
+import * as VueApolloComposable from '@vue/apollo-composable';
+import * as VueCompositionApi from '@vue/composition-api';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type ReactiveFunction<TParam> = () => TParam;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -102,4 +105,31 @@ export type ThreadsQuery = (
 );
 
 
-export const ThreadsDocument: DocumentNode<ThreadsQuery, ThreadsQueryVariables> = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"threads"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"threads"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"input"},"value":{"kind":"ObjectValue","fields":[{"kind":"ObjectField","name":{"kind":"Name","value":"closed"},"value":{"kind":"BooleanValue","value":false}}]}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"createdAt"}},{"kind":"Field","name":{"kind":"Name","value":"updatedAt"}},{"kind":"Field","name":{"kind":"Name","value":"closed"}}]}}]}}]};
+export const ThreadsDocument = gql`
+    query threads {
+  threads(input: {closed: false}) {
+    id
+    title
+    createdAt
+    updatedAt
+    closed
+  }
+}
+    `;
+
+/**
+ * __useThreadsQuery__
+ *
+ * To run a query within a Vue component, call `useThreadsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useThreadsQuery` returns an object from Apollo Client that contains result, loading and error properties
+ * you can use to render your UI.
+ *
+ * @param options that will be passed into the query, supported options are listed on: https://v4.apollo.vuejs.org/guide-composable/query.html#options;
+ *
+ * @example
+ * const { result, loading, error } = useThreadsQuery();
+ */
+export function useThreadsQuery(options: VueApolloComposable.UseQueryOptions<ThreadsQuery, ThreadsQueryVariables> | VueCompositionApi.Ref<VueApolloComposable.UseQueryOptions<ThreadsQuery, ThreadsQueryVariables>> | ReactiveFunction<VueApolloComposable.UseQueryOptions<ThreadsQuery, ThreadsQueryVariables>> = {}) {
+  return VueApolloComposable.useQuery<ThreadsQuery, ThreadsQueryVariables>(ThreadsDocument, {}, options);
+}
+export type ThreadsQueryCompositionFunctionResult = VueApolloComposable.UseQueryReturn<ThreadsQuery, ThreadsQueryVariables>;
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,22 @@
 export default {
+  ssr: false,
+  head: {
+    title: 'ness-spa',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    // link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
   components: true,
   mode: 'spa',
   storybook: {
     // Options
   },
+  buildModules: ['@nuxt/typescript-build'],
   modules: ['@nuxtjs/apollo'],
+  plugins: ['@/plugins/provide-apollo-client.js'],
   apollo: {
     clientConfigs: {
       default: '~/apollo/apolloConfig.js',
