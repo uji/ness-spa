4e87e4e665f0a9aef79a2997565cabe3a9f2f3d8
uji <ujiprog@gmail.com>
2021-01-02 07:42:55 +0000

    fix: fmt

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,7 @@
   <div class="dark">
     <div class="bg-white dark:bg-gray-800">
       <h1 class="text-gray-900 dark:text-white">Dark mode is here!</h1>
-      <p class="text-gray-600 dark:text-gray-300">
-        Lorem ipsum...
-      </p>
+      <p class="text-gray-600 dark:text-gray-300">Lorem ipsum...</p>
     </div>
   </div>
 </template>
-
