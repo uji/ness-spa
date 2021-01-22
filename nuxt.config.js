87119d9874164a73f9fe9edea11657cdae673dc0
uji <ujiprog@gmail.com>
2021-01-22 13:04:55 +0000

    feat: add icon & logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    // link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
   },
   buildModules: ['@nuxtjs/tailwindcss', '@nuxt/typescript-build', '@nuxtjs/google-fonts'],
   plugins: ['@/plugins/provide-apollo-client.js'],
--- pages/signin.vue
+++ pages/signin.vue
@@ -1,11 +1,14 @@
 <template>
-  <form @submit.prevent="signIn" class="p-8 max-w-xs mx-auto">
-    <h1 class="font-body font-bold text-black text-3xl mb-6">Sign in</h1>
-    <Input v-model="email" placeholder="email" input-type="email" class="mb-4" />
-    <Input v-model="password" placeholder="password" input-type="password" class="mb-4" />
-    <SecondaryButton type="submit" text="Sign in" class="w-full mb-4" />
-    <Button type="button" text="Sign out" @click="signOut" />
-  </form>
+  <div class="p-8 max-w-xs mx-auto">
+    <img src="~assets/svg/ness-logo.svg" class="mx-auto h-12 pr-4 mb-8"/>
+    <form @submit.prevent="signIn">
+      <h1 class="font-body font-bold text-black text-3xl mb-6">Sign in</h1>
+      <Input v-model="email" placeholder="email" input-type="email" class="mb-4" />
+      <Input v-model="password" placeholder="password" input-type="password" class="mb-4" />
+      <SecondaryButton type="submit" text="Sign in" class="w-full mb-4" />
+      <Button type="button" text="Sign out" @click="signOut" />
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
