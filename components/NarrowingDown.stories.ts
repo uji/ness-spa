49b480ad30c36a36aeea072e3d0d7f18eaea0272
kami988 <baske.yoshi007@gmail.com>
2021-05-29 23:33:57 +0000

    add: narrowing down comp

---
+++ components/NarrowingDown.stories.ts
@@ -0,0 +1,8 @@
+import '@/assets/css/tailwind.css'
+export default {
+  title: 'Narrowing Doawn',
+}
+
+export const all = () => `
+<NarrowingDown />
+`
---
+++ components/NarrowingDown.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-2 w-48">
+    <p class="mb-2 text-right text-gray-500">絞り込み</p>
+    <Input class="rounded-full h-8" placeholder="text" type="text"></Input>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
